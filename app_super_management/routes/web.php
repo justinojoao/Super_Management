9672a2323f11424ceafa274c57efae026abbadd9
justinojoao <joaojoaojust@gmail.com>
2023-06-07 14:12:41 -0300

    implementing logic of controllers and routes

--- app_super_management/routes/web.php
+++ app_super_management/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::get($uri, $callback);*/
+
+Route::get('/', [MainController::class, 'main']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/contact', [ContactController::class, 'contact'] );
